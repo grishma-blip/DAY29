d3c6b1f94fe6410100b3035186bab2108e0f1d84
Grishma Thakare <85503530+grishma-blip@users.noreply.github.com>
2024-12-17 10:24:04 +0530

    Add files via upload

---
+++ userinputcstyle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{  
+    char str[100];
+cout<<" enter a string:";
+cin>>str;
+cout<<"you entered; " << str << endl;
+cout<<"\nenter another string : ";
+cin>> str;
+cout<<"you entered : "<< str <<endl;
+return 0;
+}
